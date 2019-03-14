8253819d43c511250fd463dd02c53da692cb5a74
Jordan Klein <jklein@Jordans-MacBook-Pro.local>
2019-03-13 23:43:11 -0400

    shiny updates

--- WVS_EVS_viz/app.R
+++ WVS_EVS_viz/app.R
@@ -1,3 +1,56 @@
+#### Analysis code
+
+# Load packages
+
+library(tidyverse)
+library(ggrepel)
+library(survey)
+library(rvest)
+library(plyr)
+library(broom)
+library(knitr)
+library(stargazer)
+
+# Import data
+
+Raw <- read.csv(unz("WVS+EVS_combined.csv.zip", "WVS+EVS_combined.csv"))
+
+W_EVS <- Raw
+
+# Clean data
+
+W_EVS <- W_EVS %>%  mutate(A124_02 = replace(A124_02, A124_02 == 0, 2), 
+                           A124_06 = replace(A124_06, A124_06 == 0, 2), 
+                           C002 = replace(C002, C002 == 2, 4)) %>% 
+  mutate(C002 = replace(C002, C002 == 3, 2)) %>% 
+  mutate(C002 = replace(C002, C002 == 4, 3))
+
+W_EVS[, c("E035", "E036", "E037", "E039", "A124_02", "A124_06", "C002", "X025", "F028")] <- 
+  select(W_EVS, E035:E037, E039, A124_02, A124_06, C002, X025, F028) %>% 
+  sapply(function(x) {
+    replace(x, x < 1, NA)
+  })
+
+# Create indices
+
+W_EVS <- W_EVS %>% mutate(ec_ideo = (((E035 + 8)/9) * ((abs(E036 - 11) + 8)/9) * 
+                                       ((abs(E037 - 11) + 8)/9) * ((abs(E039 - 11) + 8)/9))^(1/4) - 1) %>%
+  mutate(rac_ideo = ((abs(A124_02 - 2) + 1) * (abs(A124_06 - 2) + 1) * 
+                       ((abs(C002 - 4) + 1)/2)^2)^(1/4) - 1)
+
+W_EVS <- W_EVS %>% mutate(edu = X025)
+W_EVS <- W_EVS %>% mutate(relig = F028)
+
+# Split into list by country
+
+country_list <- W_EVS %>% dlply('country')
+
+# Fix name of northern ireland
+
+levels(W_EVS$country)[levels(W_EVS$country) == "North Ireland"] <- "Northern Ireland"
+
+#### Visualization
+
 # Load packages
 
 library(shiny)
