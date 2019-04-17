44750e647d0d1cf8c8fd63ba1b0faa4fdb9e7ecf
Jordan Klein <jklein@Jordans-MacBook-Pro.local>
2019-04-17 00:06:05 -0400

    ideo + age app updates

--- econ_age/app.R
+++ econ_age/app.R
@@ -16,31 +16,40 @@ library(reshape2)
 url <- getURL("https://raw.githubusercontent.com/jordan-klein/internationalLeft/master/W_EVS_clean.csv")
 W_EVS <- read.csv(text = url)
 
-filter(W_EVS, !is.na(ec_ideo) & !is.na(X003)) %>% 
+filter(W_EVS, !is.na(ec_ideo) & !is.na(X003) & !is.na(edu)) %>% 
   mutate(country = droplevels(country)) %>% 
   mutate(S020 = factor(S020, levels = c(as.character(sort(unique(S020)))))) -> W_EVS
 
 # Define UI
 
-ui <- fluidPage(
-  titlePanel("Age and Economic Ideology"),
-  sidebarLayout(position = "right", 
-                sidebarPanel(
-                  selectInput("country", label = "Country", 
-                              choices = levels(W_EVS$country), selected = "United States")),
-                mainPanel(
-                  plotOutput("scatterplot"))))
+ui <- navbarPage("Model selection", 
+                 tabPanel("Not adjusted for education", 
+                          titlePanel("Age and Economic Ideology"), 
+                          sidebarLayout(position = "right", 
+                                        sidebarPanel(
+                                          selectInput("country", label = "Country", 
+                                                      choices = levels(W_EVS$country), 
+                                                      selected = "United States")), 
+                                        mainPanel(plotOutput("n_edu_plot")))), 
+                 tabPanel("Adjusted for education", 
+                          titlePanel("Age and Economic Ideology"),
+                          sidebarLayout(position = "right", 
+                                        sidebarPanel(
+                                          selectInput("country", label = "Country", 
+                                                      choices = levels(W_EVS$country), 
+                                                      selected = "United States")), 
+                                        mainPanel(plotOutput("edu_plot")))))
 
 # Define server
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   filtered <- reactive({
     filter(W_EVS) %>% 
       filter(country == input$country)
   })
   
-  output$scatterplot <- renderPlot({
+  output$n_edu_plot <- renderPlot({
     filtered() %>% 
       ggplot(aes(x = X003, y = ec_ideo, colour = S020)) + 
       geom_smooth(method = "glm", aes(weight = S017)) + 
@@ -49,7 +58,21 @@ server <- function(input, output) {
       scale_y_continuous("Economic Ideology (left to right)", breaks = c(0, .25, .5, .75, 1)) + 
       theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15)) + 
       coord_cartesian(xlim = c(20, 80), ylim = c(0, 1)) 
-      })
+  })
+  
+  output$edu_plot <- renderPlot({
+    filtered() %>% 
+      ggplot(aes(x = resid(lm(X003 ~ edu, weights = S017)), 
+                 y = resid(lm(ec_ideo ~ edu, weights = S017)), colour = S020)) + 
+      geom_smooth(method = "glm") + 
+      labs(caption = "Partial multiple linear regression plots adjusted for education, stratified by survey wave", 
+           colour = "Survey wave (year)") + 
+      scale_x_continuous("Age (young to old, residuals)", breaks = c(-45, -30, -15, 0, 15, 30, 45)) + 
+      scale_y_continuous("Economic Ideology (left to right, residuals)", breaks = c(-.5, -.25, 0, .25, .5)) + 
+      theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15)) + 
+      coord_cartesian(xlim = c(-30, 30), ylim = c(-.5, .5)) 
+    })
+  
 }
 
 # Run the application 
--- rac_age/app.R
+++ rac_age/app.R
@@ -16,31 +16,40 @@ library(reshape2)
 url <- getURL("https://raw.githubusercontent.com/jordan-klein/internationalLeft/master/W_EVS_clean.csv")
 W_EVS <- read.csv(text = url)
 
-filter(W_EVS, !is.na(RacRes_n) & !is.na(X003)) %>% 
+filter(W_EVS, !is.na(RacRes_n) & !is.na(X003) & !is.na(edu)) %>% 
   mutate(country = droplevels(country)) %>% 
   mutate(S020 = factor(S020, levels = c(as.character(sort(unique(S020)))))) -> W_EVS
 
 # Define UI
 
-ui <- fluidPage(
-  titlePanel("Age and Racial Resentment"),
-  sidebarLayout(position = "right", 
-                sidebarPanel(
-                  selectInput("country", label = "Country", 
-                              choices = levels(W_EVS$country), selected = "United States")),
-                mainPanel(
-                  plotOutput("scatterplot"))))
+ui <- navbarPage("Model selection", 
+                 tabPanel("Not adjusted for education", 
+                          titlePanel("Age and Racial Resentment"), 
+                          sidebarLayout(position = "right", 
+                                        sidebarPanel(
+                                          selectInput("country", label = "Country", 
+                                                      choices = levels(W_EVS$country), 
+                                                      selected = "United States")), 
+                                        mainPanel(plotOutput("n_edu_plot")))), 
+                 tabPanel("Adjusted for education", 
+                          titlePanel("Age and Racial Resentment"),
+                          sidebarLayout(position = "right", 
+                                        sidebarPanel(
+                                          selectInput("country", label = "Country", 
+                                                      choices = levels(W_EVS$country), 
+                                                      selected = "United States")), 
+                                        mainPanel(plotOutput("edu_plot")))))
 
 # Define server
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   filtered <- reactive({
     filter(W_EVS) %>% 
       filter(country == input$country)
   })
   
-  output$scatterplot <- renderPlot({
+  output$n_edu_plot <- renderPlot({
     filtered() %>% 
       ggplot(aes(x = X003, y = RacRes_n, colour = S020)) + 
       geom_smooth(method = "glm", aes(weight = S017)) + 
@@ -50,6 +59,20 @@ server <- function(input, output) {
       theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15)) + 
       coord_cartesian(xlim = c(20, 80), ylim = c(0, 1)) 
   })
+  
+  output$edu_plot <- renderPlot({
+    filtered() %>% 
+      ggplot(aes(x = resid(lm(X003 ~ edu, weights = S017)), 
+                 y = resid(lm(RacRes_n ~ edu, weights = S017)), colour = S020)) + 
+      geom_smooth(method = "glm") + 
+      labs(caption = "Partial multiple linear regression plots adjusted for education, stratified by survey wave", 
+           colour = "Survey wave (year)") + 
+      scale_x_continuous("Age (young to old, residuals)", breaks = c(-45, -30, -15, 0, 15, 30, 45)) + 
+      scale_y_continuous("Racial Resentment (low to high, residuals)", breaks = c(-.5, -.25, 0, .25, .5)) + 
+      theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15)) + 
+      coord_cartesian(xlim = c(-30, 30), ylim = c(-.5, .5)) 
+  })
+  
 }
 
 # Run the application 
