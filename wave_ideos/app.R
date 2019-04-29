c15873d246e6d909a1afa6cdc2d8fc31a259cac8
Jordan Klein <jklein@Jordans-MBP.home>
2019-04-29 16:06:09 -0400

    app edit & markups

--- wave_ideos/app.R
+++ wave_ideos/app.R
@@ -22,7 +22,9 @@ W_EVS <- read.csv(text = url)
 
 cw_list <- W_EVS %>% dlply('country_wave')
 
-# Create coefficient matrix
+## Create coefficient matrices
+
+# self-ideo
 
 lapply(cw_list, function(x) {
   filter(x, !is.na(self_ideo) & !is.na(ec_ideo))
@@ -58,7 +60,45 @@ left_join(mutate(id_ec_rac, country_wave = factor(country_wave, levels = combine
           mutate(unique(dplyr::select(W_EVS, country_wave, country, S020)), 
                  country_wave = factor(country_wave, levels = combined))) -> id_ec_rac
 
-## Create separate variables for each wave
+# vote ideo
+
+lapply(cw_list, function(x) {
+  filter(x, !is.na(vote_ideo) & !is.na(ec_ideo))
+}) %>% 
+  .[sapply(., function(x) {
+    dim(x)[1] > 0
+  })] %>% 
+  lapply(function(x) {
+    corr <- weightedCorr(x$ec_ideo, x$vote_ideo, "Pearson", x$S017)
+    return(data.frame(v_id_econ = corr))
+  }) %>% 
+  do.call(rbind.data.frame, .) %>% 
+  data.frame(country_wave = row.names(.), .) -> v_id_econ
+
+lapply(cw_list, function(x) {
+  filter(x, !is.na(vote_ideo) & !is.na(RacRes_n))
+}) %>% 
+  .[sapply(., function(x) {
+    dim(x)[1] > 0
+  })] %>% 
+  lapply(function(x) {
+    corr <- weightedCorr(x$RacRes_n, x$vote_ideo, "Pearson", x$S017)
+    return(data.frame(v_id_rac = corr))
+  }) %>% 
+  do.call(rbind.data.frame, .) %>% 
+  data.frame(country_wave = row.names(.), .) -> v_id_rac
+
+vote_ec_rac <- inner_join(v_id_econ, v_id_rac)
+
+combined_v <- sort(union(levels(W_EVS$country_wave), levels(vote_ec_rac$country_wave)))
+
+left_join(mutate(vote_ec_rac, country_wave = factor(country_wave, levels = combined_v)), 
+          mutate(unique(dplyr::select(W_EVS, country_wave, country, S020)), 
+                 country_wave = factor(country_wave, levels = combined_v))) -> vote_ec_rac
+
+id_viz_vote <- vote_ec_rac %>% mutate(country = droplevels(country))
+
+## Create separate variables for each wave- self ideo
 
 id_list <- id_ec_rac %>% dlply("country")
 
@@ -91,19 +131,33 @@ left_join(id_ec_rac, econ_waves) %>%
 
 id_viz <- id_viz %>% mutate(country = droplevels(country))
 
+## App
+
 # UI
 
-ui <- fluidPage(
-  titlePanel("Correlation of Economic Ideology vs. Racial Resentment with Ideological Self-Placement"), 
-  sidebarLayout(position = "right", 
-                sidebarPanel(
-                  checkboxGroupInput("checkGroup", label = "Country", 
-                                     choices = levels(id_viz$country), selected = c("Poland", "Czech Republic", 
-                                                                                    "United States", "Sweden", 
-                                                                                    "Mexico", "Germany")), 
-                  width = 2), 
-                mainPanel(ggiraphOutput("plot"), width = 10))
-)
+ui <- navbarPage("Ideology", 
+                 tabPanel("Self-Placement", 
+                          titlePanel("Correlation of Economic Ideology vs. Racial Resentment with Ideological Self-Placement"), 
+                          sidebarLayout(position = "right", 
+                                        sidebarPanel(
+                                          checkboxGroupInput("checkGroup", label = "Country", 
+                                                             choices = levels(id_viz$country), 
+                                                             selected = c("Poland", "Czech Republic", 
+                                                                          "United States", "Sweden", 
+                                                                          "Mexico", "Germany")), 
+                                          width = 2),
+                                        mainPanel(ggiraphOutput("plot"), width = 10))), 
+                 tabPanel("Voting", 
+                          titlePanel("Correlation of Economic Ideology vs. Racial Resentment with Voting Ideology"), 
+                          sidebarLayout(position = "right", 
+                                        sidebarPanel(
+                                          checkboxGroupInput("checkGroup_vote", label = "Country", 
+                                                             choices = levels(id_viz_vote$country), 
+                                                             selected = c("Poland", "Czech Republic", 
+                                                                          "Sweden", "Germany")), 
+                                          width = 2),
+                                        mainPanel(ggiraphOutput("plot_vote"), width = 10))))
+
 
 # Server
 
@@ -157,6 +211,34 @@ server <- function(input, output) {
     girafe(code = print(gg_plot), width_svg = 14, height_svg = 7)
   })
   
+  vote_data <- reactive({
+    dat_v <- filter(id_viz_vote)
+    if (is.null(dat_v))
+      return(NULL)
+    if (!is.null(input$checkGroup_vote))
+      dat_v <- dat_v[dat_v$country %in% input$checkGroup_vote, ]
+    
+    return(dat_v)
+  })
+  
+  output$plot_vote <- renderggiraph({
+    vote_data() %>% 
+      ggplot(aes(x = v_id_econ, y = v_id_rac, color = country)) + 
+      geom_point_interactive(aes(tooltip = country_wave), size = 3) + 
+      scale_color_manual(values = c(rainbow_hcl(length(unique(id_viz_vote$country)))), 
+                         name = "Country", drop = F) +
+      labs(caption = "Scatterplot") +
+      scale_x_continuous("Correlation of economic ideology with voting intention", 
+                         breaks = c(-.1, 0, .1, .2, .3, .4, .5, .6)) + 
+      scale_y_continuous("Correlation of racial resentment with voting intention", breaks = c(-.1, 0, .1, .2, .3, .4)) + 
+      theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15), 
+            legend.title = element_text(size = 10), legend.text = element_text(size = 8), 
+            legend.key.size = unit(.1, "inches")) + 
+      coord_cartesian(xlim = c(-.15, .65), ylim = c(-.15, .4)) -> gg_plot_v    
+    
+    girafe(code = print(gg_plot_v), width_svg = 14, height_svg = 7)
+  })
+
 }
 
 # Run the application 
